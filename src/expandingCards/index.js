93868c367c60695a51d976dffe5c8bf4661678b7
Glory Tefe <glorytefe@gmail.com>
2023-03-29 20:51:11 +0100

    update

--- src/expandingCards/index.js
+++ src/expandingCards/index.js
@@ -15,25 +15,3 @@ const removeActiveClass = () => {
         }
     })
 }
-
-// let arr = [1, 4, 2, 3, 8];
-// const bubbleSort = (arr) =>{
-//     let swap = true;
-//     for(const value of arr){
-//         swap = false;
-//             for(let j = 0; j < arr.length; j++){
-//                 console.log(j);
-//                 let curr = arr[j]
-//                 if(curr > arr[j + 1]){
-//                     arr[j] = arr[j + 1];
-//                     arr[j + 1] = curr
-//                     swap = true
-//                 }
-//             } 
-//             if(!swap)break;
-//         }
-//         return arr;
-// }
-// //9
-// console.log(bubbleSort(arr))
-
